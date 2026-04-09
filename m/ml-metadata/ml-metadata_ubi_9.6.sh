c288bd390b229658a2005367deabf8f45be9947b
vrushanaik <Vrusha.Naik@ibm.com>
2026-04-09 21:59:31 +0530

    added fixes

--- m/ml-metadata/ml-metadata_ubi_9.6.sh
+++ m/ml-metadata/ml-metadata_ubi_9.6.sh
@@ -31,30 +31,53 @@ SCRIPT=$(readlink -f $0)
 SCRIPT_DIR=$(dirname $SCRIPT)
 
 #Install the dependencies
-yum install -y autoconf cmake wget automake libtool zlib zlib-devel libjpeg libjpeg-devel gcc-toolset-13 python3.11 python3.11-pip python3.11-devel git unzip zip patch openssl-devel utf8proc tzdata diffutils libffi-devel
+yum install -y autoconf automake cmake libtool zlib zlib-devel libjpeg libjpeg-devel gcc-toolset-13 python3.11 python3.11-pip python3.11-devel git wget unzip zip patch diffutils openssl-devel libffi-devel utf8proc tzdata
 source /opt/rh/gcc-toolset-13/enable
 
 yum install -y java-11-openjdk-devel
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk
-export PATH=$JAVA_HOME/bin:$PATH
+export PATH="$JAVA_HOME/bin:$PATH"
 
-mkdir bazel
+mkdir -p bazel
 cd bazel
 wget https://github.com/bazelbuild/bazel/releases/download/6.5.0/bazel-6.5.0-dist.zip
 unzip bazel-6.5.0-dist.zip
 env EXTRA_BAZEL_ARGS="--tool_java_runtime_version=local_jdk" bash ./compile.sh
 cp output/bazel /usr/local/bin
-export PATH=/usr/local/bin:$PATH
+export PATH="/usr/local/bin:$PATH"
 bazel --version
-cd $wdir
+cd "$wdir"
 
 export GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=1
+python3.11 -m pip install --upgrade pip
 python3.11 -m pip install numpy pytest
 
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME/
-wget https://raw.githubusercontent.com/ppc64le/build-scripts/refs/heads/master/m/ml-metadata/ml_metadata_ubi9.6.patch
-git apply ml_metadata_ubi9.6.patch
+wget https://raw.githubusercontent.com/ppc64le/build-scripts/master/m/ml-metadata/ml_metadata_ubi9.6.patch
+
+#Fix HTML-escaped entities present IN THE PATCH ITSELF
+sed -i \
+  -e 's/&amp;/\&/g' \
+  -e 's/&lt;/</g' \
+  -e 's/&gt;/>/g' \
+  ml_metadata_ubi9.6.patch
+
+# Apply patch safely 
+git apply ml_metadata_ubi9.6.patch --reject || true
+
+#fix workspace
+#Add PostgreSQL patch only if missing
+if ! grep -q 'third_party:postgresql.patch' WORKSPACE; then
+  sed -i '/name = "postgresql"/,/)/{
+    /strip_prefix =/a\    patches = ["//ml_metadata/third_party:postgresql.patch"],
+  }' WORKSPACE
+fi
+
+#Enable ZetaSQL patch if commented
+if grep -q '#patches = \["//ml_metadata/third_party:zetasql.patch"\]' WORKSPACE; then
+  sed -i 's/#patches = \["//ml_metadata\/third_party:zetasql.patch"\]/patches = ["\/\/ml_metadata\/third_party:zetasql.patch"]/' WORKSPACE
+fi
 
 export PYTHON_BIN_PATH=$(which python3.11)
 
